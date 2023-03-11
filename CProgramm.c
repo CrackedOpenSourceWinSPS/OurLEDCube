e91d97d8719559efa8dad50c172fa9ee37954bfe
CrackedOpenSourceWinSPS <122927273+CrackedOpenSourceWinSPS@users.noreply.github.com>
2023-03-11 21:06:22 +0100

    Update CProgramm.c

--- CProgramm.c
+++ CProgramm.c
@@ -4,6 +4,7 @@
 #include <avr/io.h>
 #include <avr/pgmspace.h>
 #include <util/delay.h>
+//Library für 32 bit integers
 #include <stdint.h>
 
 #define PatternCount 3
@@ -18,7 +19,12 @@
 	first 9 bits lowest row, next second, next third
 	
 	 xxxxx 000000000 000000000 000000000
+
+    Muster des LED cubes wird in einen einzigen uint32 gespeichert, die ersten 27 bit beschreiben
+    den Zustand der LEDs, der letzte bit lässt das Muster abbrechen. Der erste bit entspricht also 
+    der LED in der niedersten ecke, während der 27. der LED in der obersten Ecke entspricht.
 */
+//Muster werden in diesem 2D array gespeichert
 const uint32_t PROGMEM Muster [3][8] =
 {
 	{
@@ -54,24 +60,34 @@ const uint32_t PROGMEM Muster [3][8] =
 	}
 };
 
+//Aktuellen LED zustand
 uint32_t led = 0;
+//Aktuelles muster im array
 int muster = 0;
+//Wird zum reseten des Knopfes benutzt
 char s = '0';
 
+//Da die LEDs immer geupdated werden müssen, wird diese methode verwendet um eine bestimmte zeit lang
+die LEDs mit dem gleichen muster zu updaten. Delay beschreibt wie langr geupdated wird.
 void Update(float delay);
 void Update (float delay) {
-		
+	//Aktuelle zeit
 	float current = 0;
 	
+        //Solange updaten bis die aktuelle zeit größer als totale zeit ist
 	while (current < delay) 
 	{		
+                //LED port reseten
 		PORTB = 0;
+                //Auf erste reihe schreiben
 		PORTD = (1<<PD3);
-		
+
+		//3 reihen
 		for (uint32_t c = 0; c < 3; c++)
 		{
 			PORTB = 0;
 			
+                        //Zustand des musters in die LEDs schreiben
 			for (uint32_t i = 0; i < 9; i++)
 			{
 				switch (i)
@@ -115,12 +131,15 @@ void Update (float delay) {
 				}
 			}
 			
+                        //Überprüfen ob schalter gedrückt
 			if ((PIND & (1 << PD2)) != 0 && s != '1')
 			{
+                                //Verhinden das schalter mehrmals muster in einem klick ändert
 				s = '1';
 				
 				muster++;
 				
+                                //Falls muster größer als array zu muster 0 zurückwechseln
 				if (muster >= PatternCount)
 					muster = 0;
 				
@@ -129,7 +148,7 @@ void Update (float delay) {
 			
 			if (s == '1' && (PIND & (1 << PD2)) == 0)
 				s = '0';
-			
+			//Kruz warten dann nächste reihe anzeigen
 			_delay_ms((float)UpdateFrame / 3.0f);
 			
 			if (c == 0)
@@ -138,6 +157,7 @@ void Update (float delay) {
 				PORTD = (1<<PD4);
 		}
 		
+                //Aktuellen zeitwert überschreiben
 		current += UpdateFrame;
 	}
 	
@@ -145,6 +165,7 @@ void Update (float delay) {
 
 int main(void)
 {
+        //Aus und eingänge setzten
 	DDRB = 0xFF;
 	DDRD = ~((1<<PD2) | (1<<PD1) | (1<<PD0));
 
@@ -153,16 +174,21 @@ int main(void)
 
     while (1) 
     {
+                //Muster schreiben
 		for (int z = 0; z < PatternLength; z++)
 		{
+                        //Zustand aus flash lesen
 			led = pgm_read_dword(&Muster[muster][z]);
 				
+                        Falls letzte bit aktiv ist das muster zu ende
 			if (led & (uint32_t)0b10000000000000000000000000000000)
 				break;
 				
+                        //Muster frame 0.3 sekunden lang anzeigen
 			Update(300.0f);
 		}
 		
+                //Led zustand reseten
 		led = 0;
     }
 }
